958c5a621e89e23f2e1461d58e10a02b75992a6f
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-06-29 20:29:29 +0530

    solved: 2026-06-29

---
+++ find-if-path-exists-in-graph.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1971 lang=java
+ *
+ * [1971] Find if Path Exists in Graph
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        return true;
+    }
+}
+// @lc code=end
+
