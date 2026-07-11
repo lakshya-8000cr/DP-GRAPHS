4d87c5af3eda9fcdbb750ccf362785bb8d32ab22
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-07-11 16:40:08 +0530

    solved: 2026-07-11

---
+++ P1/Solution15.java
@@ -0,0 +1,26 @@
+package P1;
+import java.util.*;
+
+public class Solution15 {
+    public static void main(String[] args){
+        String st = "aaaaabbbccccccccdaa";
+        HashMap<Character , Integer> mp = new LinkedHashMap<>();
+
+        int cnt = 1;
+        String ans ="";
+        for(int i=0; i<st.length(); i++){
+            if(i+1<st.length() && st.charAt(i) == st.charAt(i+1)){
+                cnt++;
+            }
+            else{
+                if(cnt==1)ans += st.charAt(i);
+                else{
+                ans += st.charAt(i);
+                ans += cnt;
+                cnt = 1;
+                }
+            }
+        }
+        System.out.print(ans);
+    }
+}
