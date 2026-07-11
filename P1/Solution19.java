c0df5d6d25f07566263e4215824f9f5382c78c26
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-07-11 17:23:11 +0530

    solved: 2026-07-11

---
+++ P1/Solution19.java
@@ -0,0 +1,33 @@
+package P1;
+import java.util.*;
+
+public class Solution19 {
+    public static void main(String[] args){
+        String e = "aaeiou";
+        HashMap<Character , Integer> mp = new HashMap<>();
+        for(int i=0; i<e.length(); i++){
+            char ch = e.charAt(i);
+            if(!mp.containsKey(ch)){
+                mp.put(ch , 1);
+            }
+            else{
+                mp.put(ch , mp.get(ch) + 1);
+            }
+        }
+
+        int max = 0;
+        char c = ' ';
+        for(char x : mp.keySet()){
+            int val = mp.get(x);
+            
+            if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
+                if(val > max){
+                    max = val;
+                    c = x;
+                }
+            }
+        }
+
+        System.out.print(c + "->" + max);
+    }
+}
