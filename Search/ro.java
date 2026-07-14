229ffc86c5297b5237c9823abc93eb54c13d4d16
lakshya-8000cr <lakshyachauhan147@gmail.com>
2026-07-14 12:24:25 +0530

    solved: 2026-07-14

--- Search/ro.java
+++ Search/ro.java
@@ -1,3 +1,23 @@
+package Search ;
 public class ro {
-    
+    public static void main(String[] args){
+        String r = "Lak";
+
+        int st = 0;
+        int end = r.length()-1;
+
+        while(st<end){
+           char c =  r.charAt(st);
+           char v =  r.charAt(end);
+
+            char temp = c;
+            c = v ;
+            v = temp;
+
+            st++;
+            end--;
+        }
+
+        System.out.print(r);
+    }
 }
